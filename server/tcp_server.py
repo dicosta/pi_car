bb3966fdca637f4e233985404c0ad0d8237a38f1
diego.costa <diego.costa@globallogic.com>
2020-11-01 00:09:24 -0300

    changed socket interface

--- server/tcp_server.py
+++ server/tcp_server.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
-import RPi.GPIO as GPIO
-import video_dir
-import car_dir
-import motor
 from socket import *
-from time import ctime          # Import necessary modules   
+from time import ctime          
+import car.car as car
 
-ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'read cpu_temp', 'home', 'distance', 'x+', 'x-', 'y+', 'y-', 'xy_home']
-
-busnum = 1          # Edit busnum to 0, if you uses Raspberry Pi 1 or 0
+ctrl_cmd = ['forward', 'backward', 'stop', 'speed_slow', 'speed_normal', 'speed_fast', 'speed_faster', 'steer_left_20', 'steer_right_20' ]
 
 HOST = ''           # The variable of HOST is null, so the function bind( ) can be bound to all valid addresses.
 PORT = 21567
@@ -16,15 +11,10 @@ BUFSIZ = 1024       # Size of the buffer
 ADDR = (HOST, PORT)
 
 tcpSerSock = socket(AF_INET, SOCK_STREAM)    # Create a socket.
-tcpSerSock.bind(ADDR)    # Bind the IP address and port number of the server. 
-tcpSerSock.listen(5)     # The parameter of listen() defines the number of connections permitted at one time. Once the 
-                         # connections are full, others will be rejected. 
-
-video_dir.setup(busnum=busnum)
-car_dir.setup(busnum=busnum)
-motor.setup(busnum=busnum)     # Initialize the Raspberry Pi GPIO connected to the DC motor. 
-video_dir.home_x_y()
-car_dir.home()
+tcpSerSock.bind(ADDR)    					 # Bind the IP address and port number of the server. 
+tcpSerSock.listen(5)     					 # The parameter of listen() defines the number of connections permitted at one time. Once the 
+                         					 # connections are full, others will be rejected. 
+mycar = car.Car()
 
 while True:
 	print 'Waiting for connection...'
@@ -37,85 +27,46 @@ while True:
 	while True:
 		data = ''
 		data = tcpCliSock.recv(BUFSIZ)    # Receive data sent from the client. 
-		# Analyze the command received and control the car accordingly.
+										  # Analyze the command received and control the car accordingly.
 		if not data:
 			break
 		if data == ctrl_cmd[0]:
-			print 'motor moving forward'
-			motor.forward()
+			print 'direction forward'			
+			car.set_direction_forward()
 		elif data == ctrl_cmd[1]:
-			print 'recv backward cmd'
-			motor.backward()
+			print 'direction backward'			
+			car.set_direction_backward()
 		elif data == ctrl_cmd[2]:
-			print 'recv left cmd'
-			car_dir.turn_left()
+			print 'stop'			
+			car.stop()
 		elif data == ctrl_cmd[3]:
-			print 'recv right cmd'
-			car_dir.turn_right()
-		elif data == ctrl_cmd[6]:
-			print 'recv home cmd'
-			car_dir.home()
+			print 'set speed slow'			
+			car.set_speed(car.MotorSpeed.SLOW)
 		elif data == ctrl_cmd[4]:
-			print 'recv stop cmd'
-			motor.ctrl(0)
+			print 'set speed normal'			
+			car.set_speed(car.MotorSpeed.NORMAL)
 		elif data == ctrl_cmd[5]:
-			print 'read cpu temp...'
-			temp = cpu_temp.read()
-			tcpCliSock.send('[%s] %0.2f' % (ctime(), temp))
+			print 'set speed fast'			
+			car.set_speed(car.MotorSpeed.FAST)
+		elif data == ctrl_cmd[6]:
+			print 'set speed faster'			
+			car.set_speed(car.MotorSpeed.FASTER)
+		elif data == ctrl_cmd[7]:
+			print 'steer left 20'			
+			car.set_steering_angle(-20)
 		elif data == ctrl_cmd[8]:
-			print 'recv x+ cmd'
-			video_dir.move_increase_x()
-		elif data == ctrl_cmd[9]:
-			print 'recv x- cmd'
-			video_dir.move_decrease_x()
-		elif data == ctrl_cmd[10]:
-			print 'recv y+ cmd'
-			video_dir.move_increase_y()
-		elif data == ctrl_cmd[11]:
-			print 'recv y- cmd'
-			video_dir.move_decrease_y()
-		elif data == ctrl_cmd[12]:
-			print 'home_x_y'
-			video_dir.home_x_y()
-		elif data[0:5] == 'speed':     # Change the speed
-			print data
-			numLen = len(data) - len('speed')
-			if numLen == 1 or numLen == 2 or numLen == 3:
-				tmp = data[-numLen:]
-				print 'tmp(str) = %s' % tmp
-				spd = int(tmp)
-				print 'spd(int) = %d' % spd
-				if spd < 24:
-					spd = 24
-				motor.setSpeed(spd)
-		elif data[0:5] == 'turn=':	#Turning Angle
+			print 'set right 20'			
+			car.set_steering_angle(20)
+
+		elif data[0:12] == 'steer_angle=':	#steering angle
 			print 'data =', data
 			angle = data.split('=')[1]
 			try:
 				angle = int(angle)
-				car_dir.turn(angle)
+				car.set_steering_angle(angle)
 			except:
 				print 'Error: angle =', angle
-		elif data[0:8] == 'forward=':
-			print 'data =', data
-			spd = data[8:]
-			try:
-				spd = int(spd)
-				motor.forward(spd)
-			except:
-				print 'Error speed =', spd
-                elif data[0:9] == 'backward=':
-                        print 'data =', data
-                        spd = data.split('=')[1]
-			try:
-				spd = int(spd)
-	                        motor.backward(spd)
-			except:
-				print 'ERROR, speed =', spd
-
 		else:
 			print 'Command Error! Cannot recognize command: ' + data
 
 tcpSerSock.close()
-
-
